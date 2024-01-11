d899693462e772901c7d69271897093f3b55780d
werdani <ammaryasser554zz@gmail.com>
2024-01-11 23:24:52 +0200

    [create] account app, contact app

---
+++ account/urls.py
@@ -0,0 +1,17 @@
+# Imports standard libraries
+#...
+
+# Imports core Django libraries
+from django.urls import path
+
+# Imports third-party libraries
+#...
+
+# Imports from your apps
+# from account import views
+
+
+urlpatterns = [
+    # path('login'),
+    
+]
---
+++ contact/urls.py
@@ -0,0 +1,20 @@
+# Imports standard libraries
+#...
+
+# Imports core Django libraries
+from django.urls import path
+
+# Imports third-party libraries
+#...
+
+# Imports from your apps
+# from contact import views
+
+
+urlpatterns = [
+    # path('list-contacts'),
+    # path('create-contacts'),
+    # path('update-contacts'),
+    # path('delete-contacts'),
+    # path('retrieve-contacts'),
+]
