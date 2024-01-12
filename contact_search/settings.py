e677194e17d92d5a3bdb3c4584bc62bbe4f88a99
werdani <ammaryasser554zz@gmail.com>
2024-01-12 19:01:12 +0200

    [add] endpoint for generate token and refresh token

--- contact_search/settings.py
+++ contact_search/settings.py
@@ -47,8 +47,13 @@ INSTALLED_APPS = [
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Contact Search',
